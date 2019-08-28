3cdfcc6fb391cf1905b9d2737d071d70f04085f2
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-28 23:03:25 +0300

    +empty task1 task2

---
+++ Garbagecollector/Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Создайте свой класс, объекты которого будут занимать много места в памяти
+//(например, в коде класса будет присутствовать большой массив) и реализуйте
+//для этого класса формализованный шаблон очистки.
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
